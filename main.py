80eb0a78b0777a55e26420f2e75859d4c883835a
Zhi Hong <56814471+tzhong0503@users.noreply.github.com>
2023-11-09 16:10:25 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(filename='main.log',level=logging.INFO)
+logging.basicConfig(filename='main.log', level=logging.INFO)
 
 # Merchant keys with their corresponding key index
 merchant_keys = {
@@ -16,53 +16,57 @@ merchant_keys = {
 used_reference_numbers = set()
 
 @app.route('/create_payment_request', methods=['POST'])
-# Create the request body
 def create_payment_request():
+
     data = request.json
 
-    logging.info(f"Incoming Data:{data}")   
-
-
-    amount = data.get('amount')
+    # log incoming data
+    logging.info(f"Incoming Data: {data}")
+    
+    # Validate incoming data
+    validate_data = ['amount', 'currency', 'description', 'payment_type']
+    if not all(key in data for key in validate_data):
+        logging.error("Validation Error: Missing required fields")
+        return jsonify({"Error": "Missing required fields"})
+    
+    amount = data['amount']
     revpay_merchant_key = "MER00000003667"
-    currency = data.get('currency')
-    description = data.get('description')
-    payment_type = data.get('payment_type')
-
+    currency = data['currency']
+    description = data['description']
+    payment_type = data['payment_type']
     reference_number = generate_unique_reference_number()
 
-    # Generate random key index with their corresponding merchant keys
     merchant_key = random.choice(list(merchant_keys.keys()))
     key_index = merchant_keys[merchant_key]
 
-    # If the payment type is credit card then set payment id = 2
+    # If payment type is credit card then set payment id = 2
     if payment_type == "credit card":
         payment_id = "2"
 
-    # Generate hash512 signature
+    # Calculate signature
     signature_value = (
         merchant_key +
-        revpay_merchant_key +  
+        revpay_merchant_key +
         reference_number +
         amount +
         currency
     )
-
+    # Hash signature
     signature = hashlib.sha512(signature_value.encode()).hexdigest()
 
     # Create the response body
     response = {
-        "Status" : "200",
+        "Status": "200",
         "Redirect_URL": "https://stg-mpg.revpay-sandbox.com.my/v1/payment",
-        "Redirect_Body":{
-        "Revpay_Merchant_ID": revpay_merchant_key,
-        "Payment_ID": payment_id,
-        "Reference_Number": reference_number,
-        "Amount": amount,
-        "Currency": currency,
-        "Transaction_Description": description,
-        "Key_Index": key_index,
-        "Signature": signature
+        "Redirect_Body": {
+            "Revpay_Merchant_ID": revpay_merchant_key,
+            "Payment_ID": payment_id,
+            "Reference_Number": reference_number,
+            "Amount": amount,
+            "Currency": currency,
+            "Transaction_Description": description,
+            "Key_Index": key_index,
+            "Signature": signature
         }
     }
 
